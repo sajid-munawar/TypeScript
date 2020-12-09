1690c3f9ed316e6a34bc357439bef26876325858
sajid-munawar <sajidmunawar666@gmail.com>
2020-12-09 07:53:52 +0500

    create typescript basic structur for learnig

---
+++ dist/index1.js
@@ -0,0 +1,18 @@
+"use strict";
+// let a:any =123
+// let b:any =["abc"]
+// let c=a+b
+// console.log(c)
+// let f:true=true;
+//  f=true
+// let a = 123
+// let b = Infinity * 0.10
+// const c = 456
+// let d = a>b
+// console.log(d) 
+// let a = 1234n 
+// let a:symbol = Symbol ["a"]
+var a = {
+    b: "Hello"
+};
+console.log(a.b);
---
+++ src/index1.ts
@@ -0,0 +1,22 @@
+// let a:any =123
+// let b:any =["abc"]
+// let c=a+b
+// console.log(c)
+
+// let f:true=true;
+//  f=true
+
+// let a = 123
+// let b = Infinity * 0.10
+// const c = 456
+// let d = a>b
+// console.log(d) 
+// let a = 1234n 
+
+// let a:symbol = Symbol ["a"]
+
+let a:{b:string} = {
+    b:"Hello"
+}
+console.log(a.b)
+console.log(a.b)
