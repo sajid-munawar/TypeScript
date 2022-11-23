a486956ce56a5aa01ad8e32acf7fe8b7eaab42a7
sajid-munawar <sajidmunawar666@gmail.com>
2022-11-23 17:21:19 +0500

    Type 'number' is not assignable to type 'string'

--- ByPanacloudLearningRepo/app.js
+++ ByPanacloudLearningRepo/app.js
@@ -4,6 +4,9 @@ let message='Hello world!';
 console.log(message);
 lett message = "Hello World";//syntax error
 console.log(message);
-*/
 let message = "Hello World";
 console.loger(message);
+*/
+let message = "Hello World";
+message = 6;
+console.log(message);
