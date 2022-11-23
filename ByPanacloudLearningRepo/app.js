2518cf4195052303cbeb7e5d81e2c700ff36199f
sajid-munawar <sajidmunawar666@gmail.com>
2022-11-23 17:19:42 +0500

    let message = "Hello World"; console.loger(message);

--- ByPanacloudLearningRepo/app.js
+++ ByPanacloudLearningRepo/app.js
@@ -2,7 +2,8 @@
 /*
 let message='Hello world!';
 console.log(message);
-*/
-lett;
-message = "Hello World"; //syntax error
+lett message = "Hello World";//syntax error
 console.log(message);
+*/
+let message = "Hello World";
+console.loger(message);
--- ByPanacloudLearningRepo/app.ts
+++ ByPanacloudLearningRepo/app.ts
@@ -1,7 +1,9 @@
 /*
 let message='Hello world!';
 console.log(message);
-*/
 lett message = "Hello World";//syntax error
 console.log(message);
+*/
+let message = "Hello World";
+console.loger(message);
         
