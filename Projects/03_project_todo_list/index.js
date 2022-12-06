7f34b1b517e9ce087fd442b0cdd3f66907d6c5b7
sajid-munawar <sajidmunawar666@gmail.com>
2022-12-06 16:34:26 +0500

    Add figlet

--- Projects/03_project_todo_list/index.js
+++ Projects/03_project_todo_list/index.js
@@ -1,19 +1,16 @@
 #! /usr/bin/env node
-
 /*
 1-Print todo list
 2-Add todo task
 3-Mark a task as complete
 3-Remove/delete a task
 */
-
-console.clear()
+console.clear();
 import chalk from 'chalk';
 import figlet from 'figlet';
-
-figlet('Todo List',(err,data)=>{
-    if(err){
+figlet('Todo List', (err, data) => {
+    if (err) {
         console.log('Something went wrong...');
     }
     console.log(chalk.green(data));
-})
+});
