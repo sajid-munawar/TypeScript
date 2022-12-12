af46748fa194dee021f1604c788ef2fe9d6b445b
sajid-munawar <sajidmunawar666@gmail.com>
2022-12-13 03:53:06 +0500

    Update project configuration

--- Projects/05_word_counter/index.ts
+++ Projects/05_word_counter/index.ts
@@ -1,3 +1,17 @@
 #! /usr/bin/env node
 
-console.log('hii');
+import chalk from 'chalk';
+import figlet from 'figlet';
+import inquirer from 'inquirer';
+// import currency_list from './currencies.js';
+
+const welcomeMsg=async()=>{
+    await figlet('Currency Converter',(err,data)=>{
+        if(err){
+            console.log('Something went wrong...');
+        }
+        console.log(chalk.yellow(data));        
+    })    
+}
+
+await welcomeMsg();
