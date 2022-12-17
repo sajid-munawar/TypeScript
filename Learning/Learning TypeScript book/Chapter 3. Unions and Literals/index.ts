89164739083d18c43ec7c673850662a859ea1532
sajid-munawar <sajidmunawar666@gmail.com>
2022-12-17 14:41:04 +0500

    Union Properties

--- Learning/Learning TypeScript book/Chapter 3. Unions and Literals/index.ts
+++ Learning/Learning TypeScript book/Chapter 3. Unions and Literals/index.ts
@@ -9,3 +9,22 @@ let thinker: string | null = null;
 if (Math.random() > 0.5) {
     thinker = "Susanne Langer"; // Ok
 }
+
+
+// Union Properties
+let physicist = Math.random() > 0.5
+? "Marie Curie"
+: 84;
+physicist.toString(); // Ok
+physicist.toUpperCase();
+//
+~~~~~~~~~~~
+// Error: Property 'toUpperCase' does not exist on type 'string | number'.
+//
+Property 'toUpperCase' does not exist on type 'number'.
+physicist.toFixed();
+//
+~~~~~~~
+// Error: Property 'toFixed' does not exist on type 'string | number'.
+//
+Property 'toFixed' does not exist on type 'string'.
--- Learning/Learning TypeScript book/Chapter 3. Unions and Literals/readme.md
+++ Learning/Learning TypeScript book/Chapter 3. Unions and Literals/readme.md
@@ -3,3 +3,9 @@ Expanding a value’s allowed type to be two or more possible types
 
 Narrowing  
 Reducing a value’s allowed type to not be one or more possible types
+
+Union Properties  
+When a value is known to be a union type, TypeScript will only allow you
+to access member properties that exist on all possible types in the union. It
+will give you a type-checking error if you try to access a type that doesn’t
+exist on all possible types.
