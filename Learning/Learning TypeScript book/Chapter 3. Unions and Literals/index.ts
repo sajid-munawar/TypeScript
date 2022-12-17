df7e6fc3ea918565c44e8380a9113b1e6559a02e
sajid-munawar <sajidmunawar666@gmail.com>
2022-12-17 14:37:07 +0500

    Declaring Union Types

--- Learning/Learning TypeScript book/Chapter 3. Unions and Literals/index.ts
+++ Learning/Learning TypeScript book/Chapter 3. Unions and Literals/index.ts
@@ -2,3 +2,10 @@
 // Union Types
 
 let mathematician = Math.random() > 0.5 ? undefined : "Mark Goldberg";
+
+// Declaring Union Types
+
+let thinker: string | null = null;
+if (Math.random() > 0.5) {
+    thinker = "Susanne Langer"; // Ok
+}
